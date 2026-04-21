18703ed448d8fc25ea602e336ca0c367c6921745
C0C2504030 <C0C2504030@edu.teu.ac.jp>
2026-04-21 16:44:24 +0900

    演習2：新作爆弾

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -28,6 +28,19 @@ def gameover(screen: pg.Surface) -> None:
     pg.display.update()
     time.sleep(5) #5秒待つ
 
+def init_bb_imgs() -> tuple[list[pg.Surface], list[int]]:
+    """時間とともに爆弾が拡大、加速する"""
+    bb_imgs = []
+    bb_accs = []
+    for r in range(1, 11):
+        bb_img = pg.Surface((20*r, 20*r))#爆弾用の空のsurfaceを作成
+        pg.draw.circle(bb_img, (255, 0, 0), (10*r, 10*r), 10*r)#爆弾円を描く
+        bb_imgs.append(bb_img)
+        bb_accs = [a for a in range(1, 11)]#加速のリストを作成
+        
+    return bb_imgs, bb_accs
+
+
 
 def check_bound(rct: pg.Rect) -> tuple[bool, bool]:
     """
@@ -59,6 +72,10 @@ def main():
     vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
+
+    bb_imgs, bb_accs = init_bb_imgs()#爆弾の大きさと加速度のリストを取得
+
+
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: 
@@ -67,6 +84,7 @@ def main():
             print("ゲームオーバー")
             gameover(screen) #ゲームオーバーの関数を呼び出す
             return #ゲームオーバーの意味でmain関数から出る
+
         screen.blit(bg_img, [0, 0]) 
         DELTA = {
             pg.K_UP: (0, -5),
@@ -93,14 +111,19 @@ def main():
         if check_bound(kk_rct) != (True, True):
             kk_rct.move_ip(-sum_mv[0], -sum_mv[1])
         screen.blit(kk_img, kk_rct)
-        bb_rct.move_ip(vx, vy) #爆弾の移動
         yoko, tate = check_bound(bb_rct)
         if not yoko : #横方向に画面外ならば
             vx *= -1 #速度の向きを反転させる
         if not tate : #縦方向に画面外ならば
             vy *= -1 #速度の向きを反転させる
+        avx = vx*bb_accs[min(tmr//500, 9)]#爆弾の加速度を設定
+        avy = vy*bb_accs[min(tmr//500, 9)]#爆弾の加速度を設定
+        bb_img = bb_imgs[min(tmr//500, 9)]#爆弾の大きさを設定
+        bb_rct.width = bb_img.get_rect().width
+        bb_rct.height = bb_img.get_rect().height
+        bb_rct.move_ip(avx, avy) #爆弾の移動
+
         screen.blit(bb_img, bb_rct) #爆弾の表示
-        
         pg.display.update()
         tmr += 1
         clock.tick(50)
