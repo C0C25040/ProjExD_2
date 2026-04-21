a58c1a033a2765647984944fccebe003581aa7aa
C0C2504030 <C0C2504030@edu.teu.ac.jp>
2026-04-21 14:25:48 +0900

    爆弾が動きだす

--- dodge_bomb.py
+++ dodge_bomb.py
@@ -1,6 +1,7 @@
 import os
 import sys
 import pygame as pg
+import random
 
 
 WIDTH, HEIGHT = 1100, 650
@@ -14,6 +15,13 @@ def main():
     kk_img = pg.transform.rotozoom(pg.image.load("fig/3.png"), 0, 0.9)
     kk_rct = kk_img.get_rect()
     kk_rct.center = 300, 200
+    bb_img = pg.Surface((20, 20))#爆弾用の空のsurfaceを作成
+    pg.draw.circle(bb_img, (255, 0, 0), (10, 10), 10)#爆弾円を描く
+    bb_rct = bb_img.get_rect()#爆弾のrectを取得
+    bb_rct.centerx = random.randint(0, WIDTH)
+    bb_rct.centery = random.randint(0, HEIGHT)#爆弾の初期座標をランダムに設定
+    # bb_img.set_colorkey((0, 0, 0))#爆弾の初期座標設定
+    vx, vy = +5, +5
     clock = pg.time.Clock()
     tmr = 0
     while True:
@@ -44,6 +52,9 @@ def main():
         #     sum_mv[0] += 5
         kk_rct.move_ip(sum_mv)
         screen.blit(kk_img, kk_rct)
+        bb_rct.move_ip(vx, vy) #爆弾の移動
+        screen.blit(bb_img, bb_rct)
+        
         pg.display.update()
         tmr += 1
         clock.tick(50)
